86a8c5e92e170e77fd78c5e9c1c6f1860ae94ac4
XiaoLLei <errant.xia@gmail.com>
2016-12-21 17:52:30 +0800

    add reader

--- c/disk_perf_test/read.c
+++ c/disk_perf_test/read.c
@@ -9,41 +9,39 @@ int main()
 
     char filename[256] = {0};
 
-    char* buff = (char*)malloc(LEN);
-
-    char* p = buff;
-    int i = 0;
-    for (i = 0; i < LEN; ++i)
-    {
-        *p = i & 0x7F;
-        ++p;
-    }
+    char* buff = (char*)malloc(LEN + 1);
 
     int time = 0;
     for (; time < 10000000; ++time)
     {
+        memset(buff, 0, LEN + 1);
+
         snprintf(filename, sizeof(filename), "./cache/data%05d", time);
 
-        disk_t* disk = disk_t_create(filename, FILE_OPEN_MOD_WRITE);
+        disk_t* disk = disk_t_create(filename, FILE_OPEN_MOD_READ);
         if (!disk)
         {
-            return -1;
+            break;
         }
 
         if (0 != disk->open(disk))
         {
             disk_t_destroy(disk);
-            return -1;
+            break;
         }
 
-        if (LEN != disk->write(disk, buff, LEN))
+        if (LEN != disk->read(disk, buff, LEN))
         {
             disk_t_destroy(disk);
-            return -1;
+            break;
         }
 
         disk_t_destroy(disk);
+
+        usleep(100);
     }
+
+    free(buff);
 
     return 0;
 }
